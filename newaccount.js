ecf2d14baa741bc6f4d09ead9f38e8e20be66867
khadijakhami298 <77051043+khadijakhami298@users.noreply.github.com>
2021-01-29 14:06:04 +0300

    Add files via upload

---
+++ newaccount.js
@@ -0,0 +1,63 @@
+/* function verifyInput(event){
+	<!--alert("input verified");-->
+	event.preventDefault();
+	verifyFirstNameNotEmpty();
+	verifyLastNameNotEmpty()
+	}
+
+function verifyNameNotEmpty(){
+ verifynf
+	let firstNameInput = document.getElementById("First name");
+	console.log(firstNameInpuyTag);
+	let value = firstNameInput.value;
+	<!--console.log(value.length);-->
+	if(value.length>=2){
+	  console.log(       */
+	  
+//https://www.formget.com/javascript-login-form/  (revision)
+
+/*
+function verifyInput(event){
+event.preventDefault();
+verifyNameNotEmpty(document.getElementById("First name").value);
+verifyNameNotEmpty(document.getElementById("Last name").value);
+verifyPassword();
+
+}
+
+function verifyNameNotEmpty(value){
+ if(value.length>=2){
+	console.log("valid Name");
+}else{
+alert("Please kit Enter a valid name");
+ }
+}
+
+
+function verifyPassword(){
+ let pwd = document.getElementById("pwd").value;
+ let cnfPwd = document.getElementById("cnf-pwd").value;
+
+ if(pwd!=cnfPwd){
+  alert("Password are not the same");
+
+}
+ if(pwd==""){
+   alert("Enter a useful password");
+}
+
+ if(pwd=="1234"){
+  alert("Enter a password other than 1234");
+}
+
+}
+
+*/
+
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+
+
+
